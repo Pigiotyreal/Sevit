3e921569563e9a16311461269b7c6de4d065a963
Pigioty <Pigiotyreal@gmail.com>
2023-09-26 21:56:55 -0400

    args

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello world!" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Please provide a file!" << std::endl;
+        return 1;
+    } else if (std::string(argv[1]).substr(std::string(argv[1]).find_last_of(".") + 1) != "sev") {
+        std::cout << "Please provide a file ending with .sev!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Compiling " << argv[1] << "...\n";
     return 0;
 }
